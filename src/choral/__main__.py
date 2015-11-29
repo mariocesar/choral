bdfba222c06c57f5a2e61d3fb2b269c63777d6d8
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2015-11-29 15:26:23 -0400

    Some branding

--- src/choral/__main__.py
+++ src/choral/__main__.py
@@ -1,7 +1,13 @@
-from gi.repository import Gtk
 from .application import Application
 
 
 if __name__ == "__main__":
+    from gi.repository import Gdk, GLib
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    Gdk.set_program_class('choral')
+    GLib.set_prgname('choral')
+
     application = Application()
     application.run(None)
--- src/choral/application.py
+++ src/choral/application.py
@@ -1,5 +1,5 @@
 import magic
-from gi.repository import Gtk
+from gi.repository import Gtk, Gio
 
 from .windows import MainWindow
 from .settings import ChoralSetttings
@@ -9,13 +9,21 @@ mime.load()
 
 
 class Application(Gtk.Application):
+
+    def __init__(self):
+        Gtk.Application.__init__(
+            self,
+            application_id='net.launchpad.choral',
+            flags=Gio.ApplicationFlags.FLAGS_NONE)
+
     def do_activate(self):
+        Gtk.Application.do_activate(self)
+
         self.settings = ChoralSetttings()
         self.theme = Gtk.IconTheme.get_default()
 
         self.window = MainWindow(self)
         self.window.show_all()
-
         self.add_window(self.window)
 
     def get_mimetype(self, path):
